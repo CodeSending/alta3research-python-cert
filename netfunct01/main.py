0e15864e168d6ff045c96e9444fde7614955d4b8
codesending <sendit512@pm.me>
2021-05-13 15:54:49 +0000

    Functions of the family

---
+++ netfunct01/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3o
+import crayons
+import time
+import yaml
+import urllib.request
+
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+def getyaml():
+    with urllib.request.urlopen('https://labs.alta3.com/courses/pyb/work2do-two') as response:
+        return yaml.load(response.read())
+#        html = response.read()
+#        work2do = yaml.load(html)
+#        return work2do
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==list
+    for coffeetime in devicecmd['todo']:
+        print('Handshaking. .. ... connecting with ' + (crayons.yellow(coffeetime['ipaddr'])))
+        time.sleep(2)
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd['todo']:
+            print(crayons.green('    Attempting to sending command --> ') + (crayons.red(mycmds['name'])) + " " + crayons.yellow(mycmds['state']) )
+            time.sleep(1)
+            # we'll learn to write code that sends cmds to device here
+    print("\n")
+
+def devicereboot(newwork):
+    for ip in newwork['todo']:
+        print('   Connecting to..' + (crayons.yellow(ip['ipaddr'])))
+    print((crayons.red('\n     REBOOTING')),end='')
+    time.sleep(1)
+    print(" . ", end='', flush=True)
+    time.sleep(3)
+    print(" . ", end='', flush=True)
+    time.sleep(3)
+    print(" . ", end='', flush=True)
+    print("NOW!")
+    time.sleep(3)
+# start our main script
+def main():
+    
+    work2do = getyaml()   
+
+    #work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
+    #["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
+    # data that replaces data stored in file
+
+    print("Welcome to the network device command pusher") # welcome message
+
+    ## get data set
+    print(crayons.green("\nData set found!!\n")) # replace with function call that reads in data from file
+    time.sleep(3)
+
+    ## run
+    commandpush(work2do) # call function to push commands to devices
+    devicereboot(work2do)
+
+# call our main function
+main()
+
